30ebe99903f31d9093ef4bc24634ac4bd556459e
unknown <y10203914@163.com>
2025-01-18 00:20:24 +0800

    2025-1-17

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -10,7 +10,7 @@ import { motion } from 'framer-motion'
 
 const TITLE = '友链'
 const DESCRIPTION = '有很多良友，胜于有很多财富。'
-const ADD_FRIEND_URL = 'https://github.com/disnox/disnox_blog/edit/main/data/friend.ts'
+const ADD_FRIEND_URL = 'https://github.com/donwhy'
 
 function SiteInfo() {
   return (
